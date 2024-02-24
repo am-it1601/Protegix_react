28e4e68371d4cd696daac8110ae3a44f24fd45a8
Amit Agarwal <amit.agarwal@ciphercru.com>
2024-02-24 19:20:46 +0530

    Added Sidebar and Navbar

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,5 +1,35 @@
+import Footer from '@/components/shared/Footer';
+import Header from '@/components/shared/Header';
+
+import Sidebar, { SidebarItem } from '@/components/shared/Sidebar';
+import { PersonIcon } from '@radix-ui/react-icons';
+
 const Home = () => {
-    return <div>Home</div>;
+    return (
+        <div className="flex">
+            <Sidebar>
+                <SidebarItem
+                    icon={<PersonIcon className="size-5" />}
+                    name="User Management"
+                    active
+                />
+                <SidebarItem
+                    icon={<PersonIcon className="size-5 " />}
+                    name="User Management"
+                    active={false}
+                />
+            </Sidebar>
+            <div className="flex flex-col flex-1 h-screen">
+                <Header />
+                <main className="container flex-1 h-screen py-4 ">
+                    <div className="h-full bg-primary">
+                        <h1>content</h1>
+                    </div>
+                </main>
+                <Footer />
+            </div>
+        </div>
+    );
 };
 
 export default Home;
