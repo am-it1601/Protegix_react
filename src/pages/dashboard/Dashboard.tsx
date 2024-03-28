a8bb167984ab195db441832d824f350151b09b02
sumit <sumit.singh@ciphercru.com>
2024-03-28 20:01:51 +0530

    dashbard card

--- src/pages/dashboard/Dashboard.tsx
+++ src/pages/dashboard/Dashboard.tsx
@@ -1,12 +1,20 @@
+import LicenseCount from '@/components/card/LicenceCount';
 import RegisterCard from '@/components/card/RegisterCard';
+import { useCompanyInfo } from '@/services';
 
 const Dashboard = () => {
+  const { data, isLoading, error } = useCompanyInfo();
+  const { registeredUsers, remainingLicense, totalLicense } = data?.data || {};
+  return (
+    <>
+      <div className="grid grid-cols-6 grid-rows-2 gap-4">
+        <div className="col-span-2 row-span-2">{registeredUsers && <RegisterCard registeredUsers={registeredUsers} />}</div>
+        <div className="col-span-2 row-span-2 col-start-3">
+          {remainingLicense && totalLicense && <LicenseCount remainingLicense={remainingLicense} totalLicense={totalLicense} />}
+        </div>
+      </div>
+    </>
+  );
+};
 
-    return (
-      <>
-        <RegisterCard/>
-      </>
-    );
-  };
-  
-  export default Dashboard;
+export default Dashboard;
